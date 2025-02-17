d482abdb4a1639ad084401fcda3b392e89448aef
Ahmed Maher <ahmaher04@gmail.com>
2025-02-17 14:46:53 +0200

    edit web details page

--- lib/features/details/presentation/web_details_page.dart
+++ lib/features/details/presentation/web_details_page.dart
@@ -32,7 +32,7 @@ class ProductDetailsWeb extends StatelessWidget {
                     flex: 1,
                     child: ClipRRect(
                       borderRadius: BorderRadius.circular(16),
-                      child: Image.network(state.selectedImage, fit: BoxFit.contain, width: 300, height: 400,
+                      child: Image.network(state.selectedImage, fit: BoxFit.cover,
                       headers: {'Access-Control-Allow-Origin': '*'},
                       ),
                     ),
@@ -40,76 +40,78 @@ class ProductDetailsWeb extends StatelessWidget {
                   const SizedBox(width: 32),
                   Expanded(
                     flex: 1,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(state.product.product_category, style: const TextStyle(fontSize: 16, color: Colors.grey)),
-                        const SizedBox(height: 8),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(state.product.product_name, style: const TextStyle(fontSize: 32, fontWeight: FontWeight.bold)),
-                            Column(
-                              children: [
-                                ElevatedButton(onPressed: cubit.increaseQuantity, child: const Icon(Icons.add)),
-                                Text('${state.quantity}', style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                                ElevatedButton(onPressed: cubit.decreaseQuantity, child: const Icon(Icons.remove)),
-                              ],
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 24),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                             Text(AppLocalizations.of(context)!.price, style: TextStyle(fontSize: 18, color: Colors.black)),
-                            Text('${state.product.product_price} ${AppLocalizations.of(context)!.egp}', style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-                          ],
-                        ),
-                        const SizedBox(height: 32),
-                        GridView.builder(
-                          shrinkWrap: true,
-                          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4, crossAxisSpacing: 16, mainAxisSpacing: 16),
-                          itemCount: state.product.product_images?.length,
-                          itemBuilder: (context, index) {
-                            String imageUrl = state.product.product_images![index];
-                            return GestureDetector(
-                              onTap: () => cubit.selectImage(imageUrl),
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  border: Border.all(color: state.selectedImage == imageUrl ? Colors.blue : Colors.transparent, width: 3),
-                                  borderRadius: BorderRadius.circular(12),
-                                ),
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(12),
-                                  child: Image.network(imageUrl, fit: BoxFit.fill, width: 50, height: 50),
-                                ),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(state.product.product_category, style: const TextStyle(fontSize: 16, color: Colors.grey)),
+                          const SizedBox(height: 8),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(state.product.product_name, style: const TextStyle(fontSize: 32, fontWeight: FontWeight.bold)),
+                              Column(
+                                children: [
+                                  ElevatedButton(onPressed: cubit.increaseQuantity, child: const Icon(Icons.add)),
+                                  Text('${state.quantity}', style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                                  ElevatedButton(onPressed: cubit.decreaseQuantity, child: const Icon(Icons.remove)),
+                                ],
                               ),
-                            );
-                          },
-                        ),
-                        const SizedBox(height: 16),
-                         Text(AppLocalizations.of(context)!.description, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                        Text(state.product.product_description, style: const TextStyle(fontSize: 16, color: Colors.black)),
-                        const SizedBox(height: 24),
-                        CustomButton(
-                          text: AppLocalizations.of(context)!.addCart,
-                          onPressed: () {
-                            final item = CartItem(
-                              id: state.product.id!,
-                              name: state.product.product_name,
-                              image: state.product.product_images!.first,
-                              basePrice: state.product.product_price,
-                              quantity: state.quantity,
-                            );
-                            context.read<CartCubit>().addItemToCart(item,context);
-                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(AppLocalizations.of(context)!.addedToCart),
-                              action: SnackBarAction(label: AppLocalizations.of(context)!.cart, onPressed: () {
-                                Navigator.of(context).push(MaterialPageRoute(builder: (context) => CartRespo(),));
-                              },),
-                            ));                          },
-                        ),
-                      ],
+                            ],
+                          ),
+                          const SizedBox(height: 24),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                               Text(AppLocalizations.of(context)!.price, style: TextStyle(fontSize: 18, color: Colors.black)),
+                              Text('${state.product.product_price} ${AppLocalizations.of(context)!.egp}', style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+                            ],
+                          ),
+                          const SizedBox(height: 32),
+                          GridView.builder(
+                            shrinkWrap: true,
+                            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4, crossAxisSpacing: 16, mainAxisSpacing: 16),
+                            itemCount: state.product.product_images?.length,
+                            itemBuilder: (context, index) {
+                              String imageUrl = state.product.product_images![index];
+                              return GestureDetector(
+                                onTap: () => cubit.selectImage(imageUrl),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    border: Border.all(color: state.selectedImage == imageUrl ? Colors.blue : Colors.transparent, width: 3),
+                                    borderRadius: BorderRadius.circular(12),
+                                  ),
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(12),
+                                    child: Image.network(imageUrl, fit: BoxFit.cover,width: 50,height: 50, ),
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                          const SizedBox(height: 16),
+                           Text(AppLocalizations.of(context)!.description, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                          Text(state.product.product_description, style: const TextStyle(fontSize: 16, color: Colors.black)),
+                          const SizedBox(height: 24),
+                          CustomButton(
+                            text: AppLocalizations.of(context)!.addCart,
+                            onPressed: () {
+                              final item = CartItem(
+                                id: state.product.id!,
+                                name: state.product.product_name,
+                                image: state.product.product_images!.first,
+                                basePrice: state.product.product_price,
+                                quantity: state.quantity,
+                              );
+                              context.read<CartCubit>().addItemToCart(item,context);
+                              ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(AppLocalizations.of(context)!.addedToCart),
+                                action: SnackBarAction(label: AppLocalizations.of(context)!.cart, onPressed: () {
+                                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => CartRespo(),));
+                                },),
+                              ));                          },
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ],
